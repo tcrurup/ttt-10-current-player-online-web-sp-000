8b585382c0cce82b7574043caa0151049246c5b0
Anthony Rurup <tcrurup@gmail.com>
2019-04-18 17:46:19 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,6 @@
 def turn_count(board)
   count = 0;
   board.each do |board_space|
-    if board_space == "X" || board_space == "O" do
-      count += 1
-    end
+    if board_space == 'X' || board_space == "O"{count += 1}
   end
 end
